c1174b109530518cdf1c12dfadc749ab9f159167
Guilherme Batista <gb400464@gmail.com>
2026-08-05 17:41:54 -0300

    AUla 118 - Import/Export

---
+++ secao9/aula118/src/index.js
@@ -0,0 +1,8 @@
+import { nome2 } from "./modulo1";
+import exportPadrao from "./modulo1"; 
+import * as tudo from "./modulo1";
+console.log(nome2);
+
+console.log(tudo);
+
+console.log(exportPadrao(10, 5));
---
+++ secao9/aula118/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Guilherme';
+const sobrenome = 'Bastista';
+const idade = 30;
+
+export default function soma(x, y){
+  return x + y;
+}
+
+export { nome as nome2};
